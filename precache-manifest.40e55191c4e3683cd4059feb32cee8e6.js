dc6502e8c63acae77b5a80a547f7b153da05454f
puer <vova@mytnik.net>
2020-04-14 22:31:24 +0300

    Updates

---
+++ precache-manifest.40e55191c4e3683cd4059feb32cee8e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef4606b8bbaa3f18210861d8f2d8102",
+    "url": "/cat_test/index.html"
+  },
+  {
+    "revision": "1f01c9ab19f08567e99a",
+    "url": "/cat_test/static/js/2.0401d4ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cat_test/static/js/2.0401d4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8966096c1227b801becc",
+    "url": "/cat_test/static/js/main.1961aaca.chunk.js"
+  },
+  {
+    "revision": "8a0c8c559f4fdd3945db",
+    "url": "/cat_test/static/js/runtime-main.c2f5a05c.js"
+  }
+]);
